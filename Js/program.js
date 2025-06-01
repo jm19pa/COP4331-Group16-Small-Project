e36c6b72113f146266df908d5960cb67d06cbdd8
TakiTyler <148297273+TakiTyler@users.noreply.github.com>
2025-06-01 09:10:06 -0400

    HTML, CSS, and JS updates

--- Js/program.js
+++ Js/program.js
@@ -14,12 +14,23 @@ function checkLogin(){
     firstName = "";
     lastName = "";
     
+    document.getElementById("error_display").innerHTML = "";
+
     // gather's the inputted username and password
     let login = document.getElementById("username_input").value;
     let pass = document.getElementById("password_input").value;
   	// let hash = md5(pass);
 
-    document.getElementById("error_display").innerHTML = "";
+    let isBlank = false;
+
+    if(checkBlank(login, "username_input") == true) isBlank = true;
+    if(checkBlank(pass, "password_input") == true) isBlank = true;
+  
+    if(isBlank == true){
+        login.reset();
+        return;
+    }
+
     console.log("Login = " + login + "\nPass = " + pass); // debugging
 
     // creating an object from the login credentials
@@ -83,16 +94,17 @@ function readCookie(){
         else if(tokens[0] == "lastName"){
             lastName = tokens[1];
         }
-        else if(tokens[0] == "userID"){
+        else if(tokens[0] == "userId"){
             userId = parseInt(tokens[1].trim());
         }
     }
 
     if(userId < 0){
-        wondow.location.href = "index.html";
+        window.location.href = "index.html";
     }
     else{
-        document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+        // document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+        console.log(userId);
     }
 }
 
@@ -110,17 +122,38 @@ function addInformation(){
     // display in-order
 }
 
+function checkBlank(inp, location){
+    let label = location + "_label";
+    document.getElementById(label).innerHTML = document.getElementById(label).innerHTML.replace("*", "").replace(" ", "");
+    document.getElementById(label).style.color = "white";
+    if(inp == ""){
+        document.getElementById("error_display").innerHTML = "Fill out the forms in red marked by *";
+        document.getElementById(label).style.color = "darkred";
+        document.getElementById(label).innerHTML += " *";
+        return true;
+    }
+    else return false;
+}
+
 function register()
 {
 
     console.log("register has been clicked");
+	document.getElementById("error_display").innerHTML = "";
 
 	let first = document.getElementById("first_name").value;
     let last = document.getElementById("last_name").value;
     let login = document.getElementById("username_input").value;
     let pass = document.getElementById("password_input").value;
+
+    let isBlank = false;
+
+    if(checkBlank(first, "first_name") == true) isBlank = true;
+    if(checkBlank(last, "last_name") == true) isBlank = true;
+    if(checkBlank(login, "username_input") == true) isBlank = true;
+    if(checkBlank(pass, "password_input") == true) isBlank = true;
   
-	document.getElementById("error_display").innerHTML = "";
+    if(isBlank == true) return;
 
     //json object for the new user being added
 	let tmp = {FirstName:first, LastName:last, Login:login, Password:pass};
@@ -149,7 +182,113 @@ function register()
 	{
         // window.alert("An error has occured, please try again");
         document.getElementById("error_display").innerHTML = "An error has occured, please try again";
-
 	}
-  
+}
+
+function addContact(){
+    let first = document.getElementById("first_name").value;
+    let last = document.getElementById("last_name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;  
+
+    let isBlank = false;
+
+    if(checkBlank(first, "first_name") == true) isBlank = true;
+    if(checkBlank(last, "last_name") == true) isBlank = true;
+    if(checkBlank(phone, "phone") == true) isBlank = true;
+    if(checkBlank(email, "email") == true) isBlank = true;
+
+    if(isBlank == true) return;
+
+    readCookie();
+
+    console.log(first + " " + last + " " + phone + " " + email + " " + userId);
+
+    let tmp = {FirstName:first, LastName:last, Email:email, Phone:phone, UserID:userId};
+    let jsonPayload = JSON.stringify( tmp );
+
+    let url = urlBase + '/add.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        xhr.onreadystatechange = function() 
+        {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            console.log("Contact has been added");
+            // Clear input fields in form 
+            document.getElementById("first_name").value = "";
+            document.getElementById("last_name").value = "";
+            document.getElementById("phone").value = "";
+            document.getElementById("email").value = "";
+            // reload contacts table and switch view to show
+            loadContacts();
+        }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        document.getElementById("error_display").innerHTML = "An error has occured, please try again";
+    }
+        
+}
+
+function loadContacts() {
+
+    readCookie();
+
+    let tmp = { search: "", UserID: userId};
+
+    let jsonPayload = JSON.stringify(tmp);
+    let url = urlBase + '/search.' + extension;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try {
+        xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let jsonObject = JSON.parse(xhr.responseText);
+            console.log(jsonObject.results);
+            if (jsonObject.error) {
+                console.log(jsonObject.error);
+                return;
+            }
+
+            // Get reference to the table
+            let table = document.getElementById("contactTable");
+
+            // Delete all rows except the header (index 0)
+            while (table.rows.length > 1) {
+                table.deleteRow(1); 
+            }
+
+
+            for (let i = 0; i < jsonObject.results.length; i++) {
+                let contact = jsonObject.results[i];
+
+                let newRow = document.createElement("tr");
+
+                Object.values(contact).forEach((value) => {
+                    let cell = document.createElement("td");
+                    cell.innerText = value;
+                    newRow.appendChild(cell);
+                })
+                
+                table.appendChild(newRow);
+
+                // Optional: Store the ID for later use (e.g., in edit/delete functions)
+                ids[i] = contact.ID;
+            }
+        }
+        };
+
+        xhr.send(jsonPayload);
+    } catch (err) {
+        console.log(err.message);
+    }
 }
